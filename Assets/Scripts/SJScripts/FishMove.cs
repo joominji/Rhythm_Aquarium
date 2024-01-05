2338ad97b8088dfba47e8c8204c462ffbbc0fd43
seungjun <149456880+el1win2@users.noreply.github.com>
2024-01-05 20:56:54 +0900

    [Add] Whale Prefab
    고래 애니메이션 및 프리팹 추가.
    고래의 추가에 맞춰 FishMove 업데이트.

--- Assets/Scripts/SJScripts/FishMove.cs
+++ Assets/Scripts/SJScripts/FishMove.cs
@@ -22,7 +22,13 @@ public class FishMove : MonoBehaviour
         directionX = 0.01f;
         directionY = 0.01f;
         Speed = 0.01f;
-        GetComponent<SpriteRenderer>().flipX = true;
+        if (gameObject.CompareTag("Wahle"))
+        { 
+            GetComponent<SpriteRenderer>().flipX = false;
+        } else
+        {
+            GetComponent<SpriteRenderer>().flipX = true;
+        }
         InvokeRepeating("PositionSetting", 0.1f, 2f); // 이동 한계 거리를 2초마다 변경
     }
 
@@ -32,23 +38,77 @@ public class FishMove : MonoBehaviour
         if (transform.position.x > PlusX)
         {
             directionX = 0-Speed;
-            if(Speed != 0.01f) GetComponent<SpriteRenderer>().flipX = false; // 느리게 움직이지 않고 반대쪽으로 이동할 때 머리를 왼쪽으로 전환
+
+            // 느리게 움직이지 않고 반대쪽으로 이동할 때 머리를 왼쪽으로 전환
+            if (Speed != 0.01f)
+            {
+                if (gameObject.CompareTag("Wahle")) // 고래는 머리가 반대.
+                {
+                    GetComponent<SpriteRenderer>().flipX = true;
+                }
+                else
+                {
+                    GetComponent<SpriteRenderer>().flipX = false;
+                }
+            }  
         }
         // 한계 거리까지 닿았을 때 반대쪽인 오른쪽으로 방향 전환 후 이동
         if (transform.position.x < MinusX)
         {
             directionX = 0+Speed;
-            if (Speed != 0.01f) GetComponent<SpriteRenderer>().flipX = true; // 느리게 움직이지 않고 반대쪽으로 이동할 때 머리를 오른쪽으로 전환
+
+            // 느리게 움직이지 않고 반대쪽으로 이동할 때 머리를 오른쪽으로 전환
+            if (Speed != 0.01f)
+            {
+                if (gameObject.CompareTag("Wahle"))
+                {
+                    GetComponent<SpriteRenderer>().flipX = false;
+                }
+                else
+                {
+                    GetComponent<SpriteRenderer>().flipX = true;
+                }
+            }   
         }
-        // 한계 거리까지 닿았을 때 반대쪽인 아래로 이동
-        if (transform.position.y > PlusY)
+
+        if (gameObject.CompareTag("Wahle")) // 고래일 경우
         {
-            directionY = 0-Speed;
+            // 한계 거리까지 닿았을 때 반대쪽인 아래로 이동
+            if (transform.position.y > PlusY)
+            {
+                directionY = 0 - Speed;
+            }
+            // 한계 거리까지 닿았을 때 반대쪽인 위로 이동
+            if (transform.position.y < -3.2f)
+            {
+                directionY = 0 + Speed;
+            }
         }
-        // 한계 거리까지 닿았을 때 반대쪽인 위로 이동
-        if (transform.position.y < MinusY)
+        else if (gameObject.CompareTag("NotFish")) // 물고기가 아닐 경우
         {
-            directionY = 0+Speed;
+            // 한계 거리까지 닿았을 때 반대쪽인 아래로 이동
+            if (transform.position.y > -3.5f)
+            {
+                directionY = 0.01f;
+            }
+            // 한계 거리까지 닿았을 때 반대쪽인 위로 이동
+            if (transform.position.y < MinusY)
+            {
+                directionY = -0.01f;
+            }
+        }
+        else // 일반 물고기일 경우
+        {
+            // 한계 거리까지 닿았을 때 반대쪽인 아래로 이동
+            if (transform.position.y > PlusY)
+            {
+                directionY = 0 - Speed;
+            }
+            // 한계 거리까지 닿았을 때 반대쪽인 위로 이동
+            if (transform.position.y < MinusY)
+            {
+                directionY = 0 + Speed;
+            }
         }
         // X의 한계 이동 거리의 +와 -가 모두 0.5 이하일 경우 느리게 움직임
         if (PlusX <= 0.5f && MinusX <= 0.5f)
@@ -87,22 +147,47 @@ public class FishMove : MonoBehaviour
     // 기본 위치 설정
     private void StartPosition()
     {
-        float randomX = Random.Range(-8f, 8f);
-        float randomY = Random.Range(-4f, 3.25f);
-        transform.position = new Vector3(randomX, randomY, 0);
+        float FirstX = Random.Range(-8f, 8f);
+        float FirstY;
+        if (gameObject.CompareTag("Wahle")) // 고래일 경우
+        {
+            FirstY = Random.Range(-3.2f, 3.25f);
+        } else if (gameObject.CompareTag("NotFish")) // 물고기가 아닐 경우
+        {
+            FirstY = -3.75f;
+        } else // 일반 물고기일 경우
+        {
+            FirstY = Random.Range(-4f, 3.25f);
+        }
+        
+        transform.position = new Vector3(FirstX, FirstY, 0);
     }
 
     // 방향 수정 함수
     private void DirectionSwitch()
     {
-        if (directionX >= 0.01f)
+        if (gameObject.CompareTag("Wahle")) // 고래일 경우 머리가 반대
         {
-            GetComponent<SpriteRenderer>().flipX = true;
-        }
-        else if (directionX <= -0.01f)
+            if (directionX >= 0.01f)
+            {
+                GetComponent<SpriteRenderer>().flipX = false;
+            }
+            else if (directionX <= -0.01f)
+            {
+                GetComponent<SpriteRenderer>().flipX = true;
+            }
+        } else
         {
-            GetComponent<SpriteRenderer>().flipX = false;
+            if (directionX >= 0.01f)
+            {
+                GetComponent<SpriteRenderer>().flipX = true;
+            }
+            else if (directionX <= -0.01f)
+            {
+                GetComponent<SpriteRenderer>().flipX = false;
+            }
         }
+            
     }
 
     // 이동 한계 거리 변경 함수
@@ -111,8 +196,20 @@ public class FishMove : MonoBehaviour
         Speed = Random.Range(0.01f, 0.1f); // 속도 조절
         PlusX = transform.position.x + Random.Range(0.4f, 10f); // X좌표 + 한계값
         MinusX = transform.position.x - Random.Range(0.4f, 10f); // X좌표 - 한계값
-        PlusY = transform.position.y + Random.Range(0.4f, 8f); // Y좌표 + 한계값
-        MinusY = transform.position.y - Random.Range(0.4f, 8f); // Y좌표 - 한계값
+        if (gameObject.CompareTag("Wahle")) // 고래일 경우
+        {
+            PlusY = transform.position.y + Random.Range(0.4f, 8f); // Y좌표 + 한계값
+            MinusY = 0.1f; // Y좌표 - 한계값
+        } else if (gameObject.CompareTag("NotFish")) // 물고기가 아닐 경우
+        {
+            PlusY = 0.1f; // Y좌표 + 한계값
+            MinusY = transform.position.y - Random.Range(0.4f, 8f); // Y좌표 - 한계값
+        } else
+        {
+            PlusY = transform.position.y + Random.Range(0.4f, 8f); // Y좌표 + 한계값
+            MinusY = transform.position.y - Random.Range(0.4f, 8f); // Y좌표 - 한계값
+        }
+            
 
         // 30% 확률로 방향 전환
         Per = Random.Range(1, 11);
