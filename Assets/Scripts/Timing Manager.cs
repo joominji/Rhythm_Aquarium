a5ee7d2c973a2269ce700e7960295840493dbeac
joominji <jmj00179@naver.com>
2024-01-05 14:13:29 +0900

    [Fix] Timing Manager

--- Assets/Scripts/Timing Manager.cs
+++ Assets/Scripts/Timing Manager.cs
@@ -14,8 +14,8 @@ public class TimingManager : MonoBehaviour
         timingBoxs = new Vector2[timingRect.Length];
         for(int i = 0; i < timingRect.Length; i++)
         {
-            timingBoxs[i].Set(Center.localPosition.y - timingRect[i].localPosition.y / 2,
-                              Center.localPosition.y - timingRect[i].localPosition.y / 2);
+            timingBoxs[i].Set(Center.position.y - timingRect[i].position.y / 2,
+                              Center.position.y - timingRect[i].position.y / 2);
         }
     }
 
@@ -23,11 +23,15 @@ public class TimingManager : MonoBehaviour
     {
         for (int i = 0; i < boxNoteList.Count; i++)
         {
-            float t_notePosX = boxNoteList[i].transform.localPosition.y;
+            float t_notePosY = boxNoteList[i].transform.position.y;
             for (int y = 0; y < timingBoxs.Length; y++)
             {
-                Debug.Log("Hit" + y);
-                return;
+                if (timingBoxs[y].y <= t_notePosY && t_notePosY <= timingBoxs[y].x)
+                {
+                    Debug.Log("Hit" + y);
+                    return;
+                }
+                
             }
         }
         Debug.Log("Miss");
