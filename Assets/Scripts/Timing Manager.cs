62d965cdd83aec8dfccda18648c20968e88fc41c
joominji <jmj00179@naver.com>
2024-01-08 19:07:24 +0900

    [Fix] Button Matched

--- Assets/Scripts/Timing Manager.cs
+++ Assets/Scripts/Timing Manager.cs
@@ -7,20 +7,23 @@ public class TimingManager : MonoBehaviour
 
     [SerializeField] Transform Center = null;
     [SerializeField] Transform[] timingRect = null;
-    public GameObject note;
+    public bool isMachtedNote;
+    public GameObject[] JudgeBoxs;
     BoomEfectController boomEfectController;
+    public float ButtonX { get; set; }
 
     Vector2[] timingBoxs = null;
-    
 
-    
+
+
+
+
     void Start()
     {
-        
+
         timingBoxs = new Vector2[timingRect.Length]; // new Vector2는 x y 좌표값 아닌가요??
-     
-       // Debug.Log(Center.transform.position.y);
-        for(int i = 0; i < timingRect.Length; i++)
+
+        for (int i = 0; i < timingRect.Length; i++)
         {
             timingBoxs[i].Set(Center.position.y + timingRect[i].localScale.y / 2,
                               Center.position.y - timingRect[i].localScale.y / 2);
@@ -29,36 +32,62 @@ public class TimingManager : MonoBehaviour
             // ex) timingBoxs[0] = (최솟값 (원래 좌표값이 음수이기 때문에 +가 최댓값),최솟값)
         }
         Debug.Log(timingBoxs[0].x);
+
+
     }
-    
-    public void CheckTiming()
+    //public void Checktracks()
+    //{
+    //    for (int i = 0; i < boxNoteList.Length; i++)
+    //    {
+    //        float t_noteposx = boxNoteList[i].transform.position.x;
+    //        for (int x = 0; x < JudgeBoxs.Length; x++)
+    //        {
+    //            if (JudgeBoxs[x].transform.position.x == t_noteposx)
+    //            {
+    //                isMachtedNote = true;
+                      //return;
+
+    //            }
+    //          
+    //                isMachtedNote = false;
+    //           
+    //        }
+    //    }
+
+    //}
+    public void CheckTiming()//float x 
     {
+        
         for (int i = 0; i < boxNoteList.Count; i++)
         {
-            float t_notePosY = boxNoteList[i].transform.position.y;  // 리스트에 저장된 노트의 y좌표축 값
-            for (int y = 0; y < timingBoxs.Length; y++) //timingBox의 Length는 timingRect 개수와 일치함
+            float t_notePosY = boxNoteList[i].transform.position.y;
+            float t_notePosX = boxNoteList[i].transform.position.x;
+
+            if(boxNoteList[i].transform.position.x == ButtonX)
             {
-                
-                if (timingBoxs[y].x >= t_notePosY && timingBoxs[y].y <= t_notePosY)
-                // 필요한 조건문 : timingBox[y] = (최소x, 최대y)이기 때문에 최소 <= t_notePosY <= 최대로 설정할것
+
+                for (int y = 0; y < timingBoxs.Length; y++) //timingBox의 Length는 timingRect 개수와 일치함
                 {
-                    // 작은 범위를 제외한 나머지 y축 범위 >> 
-                    Destroy(boxNoteList[i]);
-                    boxNoteList.RemoveAt(i);
-                    Debug.Log("Hit" + y); //디버그 되는 y는 timingBoxs 안에 저장된 timingRect의 이름이어야 함
-                    //boomEfectController.CreateBoomButton = true;
-                    //boomEfectController.CreateBoomEfect();
-                    return;
+
+                    if (timingBoxs[y].x >= t_notePosY && timingBoxs[y].y <= t_notePosY)
+
+                    {
+                        //if()
+                        Destroy(boxNoteList[i]);
+                        boxNoteList.RemoveAt(i);
+                        Debug.Log("Hit" + y); //디버그 되는 y는 timingBoxs 안에 저장된 timingRect의 이름이어야 함
+                                              //boomEfectController.CreateBoomButton = true;
+                                              //boomEfectController.CreateBoomEfect();
+                        return;
+                    }
+                    //예상 필요 판정 조건
+                    //: perfectRect.y(최소 ,  최대) = t_notePosY =>  perfect
+                    // greatRect.y(최소, 최대) = t_notePosY && perfectRect.y != t_notePosY => great
                 }
-                //예상 필요 판정 조건
-                //: perfectRect.y(최소 ,  최대) = t_notePosY =>  perfect
-                // greatRect.y(최소, 최대) = t_notePosY && perfectRect.y != t_notePosY => great
-                
             }
         }
+      
         Debug.Log("Miss");
     }
 
-  
-   
 }
